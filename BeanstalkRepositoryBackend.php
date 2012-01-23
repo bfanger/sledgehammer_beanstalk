9017445a8aeb46c928590c42318bd359c15839c0
Bob Fanger <bfanger@gmail.com>
2012-01-23 23:40:13 +0100

    Implemented basic Beanstalk REST API object retrieval

---
+++ BeanstalkRepositoryBackend.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * BeanstalkBackend
+ */
+
+namespace SledgeHammer;
+
+class BeanstalkRepositoryBackend extends RepositoryBackend {
+
+	public $identifier = 'beanstalk';
+
+	private $account;
+	private $user;
+	private $password;
+
+	function __construct($account, $user = null, $password = null) {
+		$this->account = $account;
+		$this->user = $user;
+		$this->password = $password;
+
+		$this->configs = array(
+			'Repository' => new ModelConfig('Repository', array(
+				'class' => false,
+				'properties' => array(
+					'id' => 'id',
+					'name' => 'name',
+					'title' => 'title',
+					'repository_url' => 'url',
+					'vcs' => 'vcs',
+					'created_at' => 'created',
+					'last_commit_at' => 'lastCommit',
+				),
+				'belongsTo' => array(
+					'account' => array(
+						'model' => 'Account',
+						'reference' => 'account_id',
+					)
+				),
+				'backendConfig' => array(
+					'path' => 'repositories',
+					'element' => 'repository',
+				),
+			)),
+			'Account' => new ModelConfig('Account', array(
+				'properties' => array(
+					'id' => 'id',
+					'name' => 'name',
+					'third_level_domain' => 'subdomain',
+				),
+				'belongsTo' => array(
+					'owner' => array(
+						'model' => 'User',
+						'reference' => 'owner_id',
+					)
+				),
+				'backendConfig' => array(
+					'path' => 'accounts',
+					'element' => 'account',
+				),
+			)),
+			'User' => new ModelConfig('User', array(
+				'properties' => array(
+					'id' => 'id',
+					'first_name' => 'firstname',
+					'last_name' => 'lastname',
+					'email' => 'email',
+					'owner' => 'isOwner',
+					'admin' => 'isAdmin'
+				),
+				'belongsTo' => array(
+					'account' => array(
+						'model' => 'Account',
+						'reference' => 'account_id',
+					)
+				),
+				'backendConfig' => array(
+					'path' => 'users',
+					'element' => 'user',
+				),
+			))
+		);
+	}
+
+	function get($id, $config) {
+		$url = $this->getUrl($config['path'].'/'.rawurlencode($id));
+		$responseText = file_get_contents($url);
+		$response = json_decode($responseText, true);
+		return $response[$config['element']];
+	}
+
+	function all($config) {
+		$url = $this->getUrl($config['path']);
+		$responseText = file_get_contents($url);
+		$response = json_decode($responseText, true);
+		$data = array();
+		foreach ($response as $item) {
+			$data[] = $item[$config['element']];
+		}
+		return $data;
+	}
+
+	/**
+	 *
+	 * @param string $path
+	 * @param array $params
+	 * @return \SledgeHammer\URL
+	 */
+	private function getUrl($path, $params = array()) {
+		$url = new URL('http://'.$this->account.'.beanstalkapp.com/api/'.$path.'.json');
+		$url->user = $this->user;
+		$url->pass = $this->password;
+		$url->query = $params;
+		return $url;
+//
+	}
+}
+
+?>
