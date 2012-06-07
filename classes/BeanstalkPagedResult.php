8d8e4cc813c26749fa1373e31e063fbc6d7579d8
Bob Fanger <bob.fanger@noprotocol.nl>
2012-06-07 10:07:30 +0200

    Documented BeanstalkPagedResult

--- classes/BeanstalkPagedResult.php
+++ classes/BeanstalkPagedResult.php
@@ -1,29 +1,74 @@
 <?php
-namespace SledgeHammer;
 /**
- * BeanstalkPagedResult, An iterator that will fetch all records in a paged result.
- * The iterator sends the request for the next page when it is needed.
+ * BeanstalkPagedResult
+ */
+namespace Sledgehammer;
+/**
+ * Iterator that will fetch all pages for a paged API call.
+ * @todo Prefetch the next page in the background.
  *
  * @package Beanstalk
  */
 class BeanstalkPagedResult extends Object implements \Iterator {
 
+	/**
+	 * URL of the paged API call.
+	 * @var URL
+	 */
 	private $url;
+
+	/**
+	 * The name of the wrapper element.
+	 * @var string
+	 */
 	private $element;
+
+	/**
+	 * Active page.
+	 * @var int
+	 */
 	private $page = 1;
+
+	/**
+	 * Results per page (max 30)
+	 * @var int
+	 */
 	private $perPage = 30;
+
+	/**
+	 * Resultset of the API calls.
+	 * @var type
+	 */
 	private $results = array();
+
+	/**
+	 *
+	 * @var bool
+	 */
 	private $isLastPage = false;
 
+	/**
+	 * Constructor
+	 * @param URL $url URL of the paged API call.
+	 * @param string $element The name of the wrapper element.
+	 */
 	function __construct($url, $element) {
 		$this->url = $url;
 		$this->element = $element;
 	}
 
+	/**
+	 * Iterator::current()
+	 * @return mixed
+	 */
 	function current() {
 		return current($this->results[$this->page]);
 	}
 
+	/**
+	 * Iterator::key()
+	 * @return int
+	 */
 	function key() {
 		$base = ($this->page - 1) * $this->perPage;
 		$key = key($this->results[$this->page]);
@@ -32,16 +77,23 @@ class BeanstalkPagedResult extends Object implements \Iterator {
 		}
 	}
 
+	/**
+	 * Iterator::next()
+	 * @return void
+	 */
 	function next() {
 		$next = next($this->results[$this->page]);
 		if ($next === false && key($this->results[$this->page]) === null && $this->isLastPage === false) { // End of page?
 			$this->page++;
 			$this->results[$this->page] = $this->fetchPage($this->page);
 			$this->isLastPage = (count($this->results[$this->page]) != $this->perPage); // Are there less records than the page
-			return false;
 		}
 	}
 
+	/**
+	 * Iterator::rewind()
+	 * @return void
+	 */
 	function rewind() {
 		if (count($this->results) === 0) {
 			$this->results[1] = $this->fetchPage(1);
@@ -54,14 +106,19 @@ class BeanstalkPagedResult extends Object implements \Iterator {
 		$this->isLastPage = (count($this->results[1]) != $this->perPage); // Are there less records than the page
 	}
 
+	/**
+	 * Iterator::valid()
+	 * @return bool
+	 */
 	function valid() {
 		return (key($this->results[$this->page]) !== null);
 	}
 
 	/**
-	 * Retrieve the data
+	 * Execute the API call for the given page and unpack the result.
+	 *
 	 * @param int $page
-	 * @throws InfoException
+	 * @return array Resultset
 	 */
 	private function fetchPage($page) {
 		$this->url->query['page'] = $page;
